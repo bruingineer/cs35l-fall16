a2d76d32d19cd0b72189be28681c3dff7b3f45a3
JOSH CAMARENA <joshcamarena@ucla.edu>
2016-11-28 17:28:47 -0800

    add 8assign

---
+++ 8assign/cos5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char* argv[]) {
+  int val = cos(0.5);
+  printf("%.17g\n", val);
+  return 0;
+}
---
+++ 8assign/dlskeleton/randmain.mk
@@ -0,0 +1,15 @@
+OPTIMIZE = -O2
+
+CC = gcc
+CFLAGS = $(OPTIMIZE) -Wall -Wextra -g3 -march=native -mtune=native -mrdrnd
+
+randmain.o: randmain.c
+	$(CC) $(CFLAGS) randmain.c -c -o $@
+randcpuid.o: randcpuid.c
+	$(CC) $(CFLAGS) randcpuid.c -c -o $@
+randmain: randmain.o randcpuid.o
+	$(CC) $(CFLAGS) randmain.o randcpuid.o -ldl -Wl,-rpath=$(PWD) -o $@
+randlibhw.so: randlibhw.c
+	$(CC) $(CFLAGS) randlibhw.c -shared -fPIC -o $@
+randlibsw.so: randlibsw.c
+	$(CC) $(CFLAGS) randlibsw.c -shared -fPIC -o $@
